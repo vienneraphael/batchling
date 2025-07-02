c1e58f2ba99ec24fe8633d22d78f8171156def5f
Raphael Vienne <129849233+vienneraphael@users.noreply.github.com>
2025-07-02 19:19:20 +0200

    remove placeholder files

--- src/batchling/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    return 42
-
-
-if __name__ == "__main__":  # pragma: no cover
-    main()
--- tests/test_foo.py
+++
@@ -1,5 +0,0 @@
-from batchling.main import main
-
-
-def test_main():
-    assert main() == 42
