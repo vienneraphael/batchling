3385221574bdea2e73fdd1a0ee76944c15860cbd
Raphael Vienne <129849233+vienneraphael@users.noreply.github.com>
2025-07-02 22:49:30 +0200

    feat: add experiment db model

---
+++ src/batchling/db/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import JSON, Column, DateTime, Integer, String
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Experiment(Base):
+    __tablename__ = "experiments"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+    model = Column(String, nullable=False)
+    response_format = Column(JSON, nullable=True)
+    input_file_path = Column(String, nullable=True)
+    input_file_id = Column(String, nullable=True)
+    status = Column(String, nullable=False)
+    batch_id = Column(String, nullable=True)
