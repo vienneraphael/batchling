37f04116b932b1ecdd86cb439f53365ca0fc59c6
Raphael Vienne <129849233+vienneraphael@users.noreply.github.com>
2025-07-03 20:43:00 +0200

    feat: update nullabke values

--- src/batchling/db/models.py
+++ src/batchling/db/models.py
@@ -10,13 +10,13 @@ class Experiment(Base):
     __tablename__ = "experiments"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    description = Column(String, nullable=False)
+    name = Column(String, nullable=True)
+    description = Column(String, nullable=True)
     created_at = Column(DateTime, nullable=False)
     updated_at = Column(DateTime, nullable=False)
     model = Column(String, nullable=False)
     response_format = Column(JSON, nullable=True)
     input_file_path = Column(String, nullable=True)
     input_file_id = Column(String, nullable=True)
-    status = Column(String, nullable=False)
+    status = Column(String, nullable=True)
     batch_id = Column(String, nullable=True)
