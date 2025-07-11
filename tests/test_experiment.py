2f5afed2e3ea52bfe33f297780125539c77a5c90
Raphael Vienne <129849233+vienneraphael@users.noreply.github.com>
2025-07-11 22:30:54 +0200

    feat: add more tests

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -4,27 +4,7 @@ from batchling.experiment import Experiment, ExperimentStatus
 
 
 @pytest.fixture
-def experiment():
-    return Experiment(
-        id="experiment-test-1",
-        model="gpt-4o-mini",
-        name="test 1",
-        description="test experiment number 1",
-    )
-
-
-def test_invalid_input_file_path():
-    with pytest.raises(ValueError, match="input_file_path must be a .jsonl file"):
-        Experiment(
-            id="experiment-test-1",
-            model="gpt-4o-mini",
-            name="test 1",
-            description="test experiment number 1",
-            input_file_path="test.txt",
-        )
-
-
-def test_setup(tmp_path):
+def experiment(tmp_path):
     template_messages = [
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "{greeting}, how are you {name}?"},
@@ -39,6 +19,27 @@ def test_setup(tmp_path):
         template_messages=template_messages,
         placeholders=placeholders,
     )
+    return experiment
+
+
+def test_invalid_input_file_path():
+    with pytest.raises(ValueError, match="input_file_path must be a .jsonl file"):
+        Experiment(
+            id="experiment-test-1",
+            model="gpt-4o-mini",
+            name="test 1",
+            description="test experiment number 1",
+            input_file_path="test.txt",
+        )
+
+
+def test_double_setup(experiment: Experiment):
+    experiment.setup()
+    with pytest.raises(ValueError, match="Experiment in status setup is not in created status"):
+        experiment.setup()
+
+
+def test_setup(experiment: Experiment):
     experiment.setup()
     assert experiment.status == ExperimentStatus.SETUP
 
