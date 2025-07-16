6221d7fc40e0a01679d1eb2ac6e9e386faad8149
Raphael Vienne <129849233+vienneraphael@users.noreply.github.com>
2025-07-16 21:57:30 +0200

    fix: patch mock client the right way

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,19 @@
-from unittest.mock import Mock, patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 
 
 @pytest.fixture(autouse=True)
 def mock_client():
-    with patch("openai.OpenAI") as mock_client:
-        mock_client.return_value = Mock()
-        yield mock_client
+    with patch("openai.OpenAI") as OpenAI_cls:
+        client = MagicMock(name="openai_client")
+        file_obj = MagicMock(name="fake_file", id="test-file-id")
+        client.files.create.return_value = file_obj
+        batch_obj = MagicMock(name="fake_batch", id="test-batch-id")
+        client.batches.create.return_value = batch_obj
+        OpenAI_cls.return_value = client
+
+        yield client
 
 
 @pytest.fixture(autouse=True)
