743f56803e4330401b2869d3dc922fe892af4c6d
Raphael Vienne <129849233+vienneraphael@users.noreply.github.com>
2025-07-22 21:01:05 +0200

    allow base_url being None in router

--- src/batchling/cls_utils.py
+++ src/batchling/cls_utils.py
@@ -3,7 +3,7 @@ from typing import Type
 from batchling.experiment import Experiment, OpenAIExperiment
 
 
-def get_cls_from_url(base_url: str) -> Type[Experiment]:
+def get_cls_from_url(base_url: str | None = None) -> Type[Experiment]:
     if base_url:
         return OpenAIExperiment
     else:
