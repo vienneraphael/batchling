913821bee01fe07e6c5f1177ef2056471f5f739a
Raphael Vienne <129849233+vienneraphael@users.noreply.github.com>
2025-08-17 17:03:18 +0200

    feat: create request abstractions

---
+++ src/batchling/request.py
@@ -0,0 +1,37 @@
+import typing as t
+
+from pydantic import BaseModel, Field
+
+
+class Message(BaseModel):
+    role: t.Literal["system", "user", "assistant"]
+    content: str
+
+
+class Body(BaseModel):
+    messages: list[Message]
+    max_tokens: int | None = None
+
+
+class Request(BaseModel):
+    custom_id: str
+    body: Body
+
+
+class MistralBody(Body):
+    response_format: dict | None = None
+
+
+class MistralRequest(Request):
+    body: MistralBody
+
+
+class OpenAIBody(Body):
+    model: str
+    response_format: dict | None = None
+
+
+class OpenAIRequest(Request):
+    method: t.Literal["POST"] = Field(default="POST", init=False)
+    url: str
+    body: OpenAIBody
