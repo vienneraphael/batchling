f1142db0853345bb29a996076b663b673d93609a
Raphael Vienne <129849233+vienneraphael@users.noreply.github.com>
2025-08-17 16:03:12 +0200

    feat: route experiment and request types from prov

--- src/batchling/cls_utils.py
+++ src/batchling/cls_utils.py
@@ -1,10 +1,18 @@
 from typing import Type
 
 from batchling.experiment import Experiment, MistralExperiment, OpenAIExperiment
+from batchling.request import MistralRequest, OpenAIRequest, Request
 
 
-def get_cls_from_url(provider: str | None = None) -> Type[Experiment]:
+def get_experiment_cls_from_provider(provider: str | None = None) -> Type[Experiment]:
     if provider == "mistral":
         return MistralExperiment
     else:
         return OpenAIExperiment
+
+
+def get_request_cls_from_provider(provider: str | None = None) -> Type[Request]:
+    if provider == "mistral":
+        return MistralRequest
+    else:
+        return OpenAIRequest
