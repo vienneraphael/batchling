1741748284fddad2e963f6de337c7cbfed14a7d1
Raphael <raphael.vienne@live.fr>
2025-09-05 17:07:58 +0200

    expose only EM through __init__

--- src/batchling/__init__.py
+++ src/batchling/__init__.py
@@ -1,13 +1,5 @@
 from .experiment_manager import ExperimentManager as ExperimentManager
-from .providers.groq import GroqExperiment as GroqExperiment
-from .providers.mistral import MistralExperiment as MistralExperiment
-from .providers.openai import OpenAIExperiment as OpenAIExperiment
-from .providers.together import TogetherExperiment as TogetherExperiment
 
 __all__ = [
-    "OpenAIExperiment",
-    "MistralExperiment",
-    "GroqExperiment",
-    "TogetherExperiment",
     "ExperimentManager",
 ]
