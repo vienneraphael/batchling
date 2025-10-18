ddf887ca5002f6a095c2abf8d969a89946c66f87
Raphael Vienne <raphael.vienne@live.fr>
2025-10-18 11:35:32 -0700

    chore: add so results to global script

--- scripts/get_results.sh
+++ scripts/get_results.sh
@@ -6,3 +6,10 @@ batchling results openai
 batchling results together
 batchling results groq
 batchling results gemini
+
+batchling results anthropic_so
+batchling results mistral_so
+batchling results openai_so
+batchling results together_so
+batchling results groq_so
+batchling results gemini_so
